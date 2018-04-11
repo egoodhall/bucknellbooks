935faf3895d210ed4e86d186711db4a90b2287ac
Eric Marshall <emm035@bucknell.edu>
2018-04-11 11:13:58 -0400

    Added API mockup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import express from 'express';
 import morgan from 'morgan';
-import bodyParser from 'body-parser';
+import bodyParser, { text } from 'body-parser';
 
 const port = 8080;
 
@@ -19,7 +19,29 @@ app.use('/', express.static(staticDir)); // Serve static files through the root 
 // Add API routes
 const apiRouter = new express.Router();
 
-app.use('/api', apiRouter); // Mount behind the /api sub-route
+// Search
+apiRouter.get('/search'); // Attach search route
+
+// Textbooks
+const textbookRouter = new express.Router();
+textbookRouter.get('/'); // Retrieve all textbooks
+textbookRouter.post('/'); // Create a new textbook
+textbookRouter.get('/:id'); // Retrieve the textbook with id
+textbookRouter.put('/:id'); // Modify a textbook
+textbookRouter.delete('/:id'); // Delete the textbook with id
+apiRouter.use('/books', textbookRouter); // Attach textbook root
+
+// Users
+const userRouter = new express.Router();
+userRouter.get('/'); // Get all users
+userRouter.get('/:uid'); // Retrieve a user
+userRouter.post('/:uid'); // Create a user
+userRouter.put('/:uid'); // Modify a user
+userRouter.use('/:uid/books', textbookRouter); // Allow all textbook queries
+apiRouter.use('/users', userRouter); // Attach user root
+
+
+app.use('/api', apiRouter); // Mount everything behind the /api sub-route
 
 // Error handling (log it and return a 500 error)
 app.use((err, req, res, next) => {
