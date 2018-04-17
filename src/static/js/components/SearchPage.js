64feac6797b78fb3c0eabb271e60656312ed7975
Eric Marshall <emm035@bucknell.edu>
2018-04-17 13:18:26 -0400

    Fixes to search bar/search page

--- src/static/js/components/SearchPage.js
+++ src/static/js/components/SearchPage.js
@@ -1,29 +1,54 @@
 import React, {Component} from 'react';
 import WelcomeMessage from './WelcomeMessage.js';
 import SearchBar from './SearchBar.js';
-import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
+import styling from '../styling.js';
+
+const getStyles = (props, state) => ({
+  appBar: {
+    zIndex: 3,
+    height: 80,
+    justifyContent: 'center',
+    display: 'flex',
+    background: styling.colors.primary
+  },
+  searchBar: {
+    position: 'fixed',
+    top: '16px',
+    zIndex: 4,
+    width: '60em'
+  }
+});
 
 class SearchPage extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      queryText: ''
+    };
+
+    this.onQueryTextChanged = this.onQueryTextChanged.bind(this);
   }
 
-  componentWillMount() {
-  }
-
-  componentWillUpdate(nextProps, nextState) {
+  onQueryTextChanged(text) {
+    this.setState({
+      queryText: text
+    });
   }
 
   render() {
+    const styles = getStyles(this.props, this.state);
     return (
       <div>
-        <AppBar position="static">
+        <Paper elevation={4} style={styles.appBar}>
           <SearchBar
-            onChange={()=>{}}
+            style={styles.searchBar}
+            onChange={this.onQueryTextChanged}
             onRequestSearch={()=>{}}
+            value={this.state.queryText}
           />
-        </AppBar>
+        </Paper>
         <h1>This is the search page!</h1>
         <WelcomeMessage />
       </div>
