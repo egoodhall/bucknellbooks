0fc28ffda6dbdd8e394f4edd8bd5df572efa02f2
Eric Marshall <emm035@bucknell.edu>
2018-04-17 14:15:43 -0400

    Got top bar looking good

--- src/static/js/app.jsx
+++ src/static/js/app.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import Navigation from './navigation/Navigation.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-// import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 
-// Build M-UI theme for app
-// const getTheme = () => {
-// };
+const getTheme = () => {
+  let overwrites = {
+    'palette': {
+      'primary1Color': '#1976d2',
+      'primary2Color': '#1565c0',
+      'accent1Color': '#f57c00'
+    }
+  };
+  return getMuiTheme(baseTheme, overwrites);
+};
 
 class App extends React.Component {
   constructor() {
@@ -16,7 +23,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <MuiThemeProvider muiTheme={getTheme()}>
         <Navigation />
       </MuiThemeProvider>
     );
--- src/static/js/components/SearchPage.js
+++ src/static/js/components/SearchPage.js
@@ -2,22 +2,41 @@ import React, {Component} from 'react';
 import WelcomeMessage from './WelcomeMessage.js';
 import SearchBar from 'material-ui-search-bar';
 import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
+import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
 import * as Typicons from 'react-icons/lib/ti';
 import styling from '../styling.js';
+import { withRouter } from 'react-router-dom';
 
 const getStyles = (props, state) => ({
   appBar: {
     zIndex: 3,
     height: 80,
-    justifyContent: 'center',
+    background: styling.colors.primary,
+    position: 'fixed',
+    width: '100%',
     display: 'flex',
-    background: styling.colors.primary
+    alignItems: 'center',
+    padding: '16px'
   },
   searchBar: {
-    position: 'fixed',
+    flexGrow: 1,
+    float: 'left',
     top: '16px',
     zIndex: 4,
-    width: '60em'
+    width: '50%'
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    padding: 0,
+    marginRight: '24px'
+  },
+  newButton: {
+    marginLeft: '24px'
   }
 });
 
@@ -42,7 +61,19 @@ class SearchPage extends Component {
     const styles = getStyles(this.props, this.state);
     return (
       <div>
-        <Paper elevation={4} style={styles.appBar}>
+        <Paper zDepth={1} style={styles.appBar}>
+          <IconMenu
+            iconButtonElement={
+              <IconButton style={styles.avatar}>
+                <Avatar style={styles.avatar} />
+              </IconButton>
+            }
+            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          >
+            <MenuItem primaryText="My Profile" onClick={() => this.props.history.push('/profile')}/>
+            <MenuItem primaryText="Sign out" onClick={() => console.log('Log Out!')}/>
+          </IconMenu>
           <SearchBar
             closeIcon={<Typicons.TiDelete color={'#9e9e9e'} size={20}/>}
             searchIcon={<Typicons.TiZoom color={'#9e9e9e'} size={20}/>}
@@ -51,6 +82,7 @@ class SearchPage extends Component {
             onRequestSearch={()=>{}}
             value={this.state.queryText}
           />
+          <RaisedButton style={styles.newButton} secondary={true}>+ Book</RaisedButton>
         </Paper>
         <h1>This is the search page!</h1>
         <WelcomeMessage />
@@ -59,4 +91,4 @@ class SearchPage extends Component {
   }
 }
 
-export default SearchPage;
+export default withRouter(SearchPage);
